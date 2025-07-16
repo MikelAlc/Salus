5b49887b3637eeab942462d881b52f8b4ee34abe
MikelAlc <mklalcazar@gmail.com>
2025-07-16 18:37:20 -0400

    Initial Commit

---
+++ SalusTests/SalusTests.swift
@@ -0,0 +1,17 @@
+//
+//  SalusTests.swift
+//  SalusTests
+//
+//  Created by Mikel on 7/16/25.
+//
+
+import Testing
+@testable import Salus
+
+struct SalusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
